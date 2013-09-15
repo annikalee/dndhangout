4b18a3b076d089a8538cb7bc43779006034599de
Annika Peterson <annikalee@me.com>
2013-09-15 03:50:53 -0400

    Updated tokens with a list of all tokens

--- static/token.js
+++ static/token.js
@@ -11,9 +11,11 @@ var token = function(color, type, name, x, y) {
 		}
 		else {
 			gameGrid.removeToken(x, y); 
+			currentTokens.removeToken(this);
 			gameGrid.addToken(a, b); 
 			this.x = a; 
 			this.y = b;
+			currentTokens.addToken(this);
 			// Redraw 
 		}
 
@@ -24,5 +26,26 @@ var token = function(color, type, name, x, y) {
 		tokenImg.onload = function() {
       		ctx.drawImage(tokenImg, this.x, this.y, 30, 30);
 		}
+	}
+}
+
+var currentTokens = function() {
+	this.currentTokens = new Array(); 
+	this.addToken = function(token) {
+		this.currentTokens.push(token);
+	}
+	this.removeToken = function(token) {
+		for (i = 0; i < this.currentTokens.length; i++) {
+			if this.currentTokens[i] === token {
+				this.currentTokens.removeByIndex(i);
+			}
+		}
+	}
+	this.toString = function() {
+		var result = "";
+		for (i = 0; i < this.currentTokens.length; i++) {
+			result += JSON.stringify[this.currentTokens[i]];
+		}
+		return result;
 	}
 }
