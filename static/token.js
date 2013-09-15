01544bd2029ab7d929b1e60c795b08839d273b02
Annika Peterson <annikalee@me.com>
2013-09-14 23:46:11 -0400

    Token implemented

--- static/token.js
+++ static/token.js
@@ -1,8 +1,28 @@
-var token = function(color, type, name, location) {
+var token = function(color, type, name, x, y) {
 	this.color = color; 
 	this.type = type; 
 	this.name = name; 
-	this.location = location; 
+	this.x = x;
+	this.y = y; 
 	this.url = "//hangoutdnd.appspot.com/static/images/" + color + ".gif";
-	this.moveToken(x, y) = 
+	this.moveToken = function(a, b)	{
+		if (gameGrid[a][b].occupied === true) {
+			//Nope message
+		}
+		else {
+			gameGrid.removeToken(x, y); 
+			gameGrid.addToken(a, b); 
+			this.x = a; 
+			this.y = b;
+			// Redraw 
+		}
+
+	}
+	this.draw = function() {
+		var tokenImg = new Image(); 
+		tokenImg.src = this.url; 
+		tokenImg.onload = function() {
+      		ctx.drawImage(tokenImg, this.x, this.y, 30, 30);
+		}
+	}
 }
